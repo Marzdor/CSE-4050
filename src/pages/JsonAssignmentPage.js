2b9184dcdf7a4349cc4627d8163b0c335fdf936f
Max <marzdor@gmail.com>
2021-08-22 13:43:38 -0700

    added json assignment page

---
+++ src/pages/JsonAssignmentPage.js
@@ -0,0 +1,14 @@
+import BasicPageContainer from "./common/BasicPageContainer";
+import JsonStringContainer from "../components/JsonAssignment/JsonStringContainer";
+import React from "react";
+import styles from "../styles/pageStyles.module.css";
+
+const JsonAssignmentPage = () => {
+  return (
+    <BasicPageContainer>
+      <h1 className={styles["Page-Title"]}>Api Assignment Page </h1>
+    </BasicPageContainer>
+  );
+};
+
+export default JsonAssignmentPage;
